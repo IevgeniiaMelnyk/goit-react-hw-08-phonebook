e03a3aa43b8280987b0e112dfcb7c7d0df7de840
Ievgeniia Melnyk <agnikava@gmail.com>
2023-03-11 14:19:18 +0200

    add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-// import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'App';
 import { GlobalStyle } from './GlobalStyle.styled';
 import { store, persistor } from 'redux/store';
@@ -10,9 +10,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <GlobalStyle />
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      <App />
-      {/* </PersistGate> */}
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </>
 );
