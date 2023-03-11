999f85c357cfa2938f4af36f03c2521466691028
Ievgeniia Melnyk <agnikava@gmail.com>
2023-03-11 14:17:21 +0200

    add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'App';
 import { GlobalStyle } from './GlobalStyle.styled';
 import { store, persistor } from 'redux/store';
