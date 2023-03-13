e9cfa887aeb14031e4d2091a5bb08d4c2d9a0fea
Ievgeniia Melnyk <agnikava@gmail.com>
2023-03-13 10:25:43 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Provider } from 'react-redux';
 import { store } from 'redux/store';
-// import { PersistGate } from 'redux-persist/integration/react';
-// import { persistor } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from 'redux/store';
 import { BrowserRouter } from 'react-router-dom';
 import NotificationContainer from 'react-notifications/lib/NotificationContainer';
 import UserRoutes from 'UserRoutes';
@@ -10,14 +10,14 @@ import AuthLayout from 'modules/AuthLayout/AuthLayout';
 export const App = () => {
   return (
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      <AuthLayout>
-        <BrowserRouter basename="/goit-react-hw-08-phonebook">
-          <UserRoutes />
-          <NotificationContainer />
-        </BrowserRouter>
-      </AuthLayout>
-      {/* </PersistGate> */}
+      <PersistGate loading={null} persistor={persistor}>
+        <AuthLayout>
+          <BrowserRouter basename="/goit-react-hw-08-phonebook">
+            <UserRoutes />
+            <NotificationContainer />
+          </BrowserRouter>
+        </AuthLayout>
+      </PersistGate>
     </Provider>
   );
 };
--- src/UserRoutes.jsx
+++ src/UserRoutes.jsx
@@ -3,9 +3,9 @@ import { lazy, Suspense } from 'react';
 import NotificationContainer from 'react-notifications/lib/NotificationContainer';
 import PrivateRoute from 'modules/PrivateRoute/PrivateRoute';
 import PublicRoute from 'modules/PublicRoute/PublicRoute';
+import Spinner from 'shared/components/Spinner/Spinner';
+import Layout from 'modules/Layout/Layout';
 
-const Spinner = lazy(() => import('shared/components/Spinner/Spinner'));
-const Layout = lazy(() => import('modules/Layout/Layout'));
 const HomePage = lazy(() => import('pages/HomePage/HomePage'));
 const RegisterPage = lazy(() => import('pages/RegisterPage/RegisterPage'));
 const LoginPage = lazy(() => import('pages/LoginPage/LoginPage'));
